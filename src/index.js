4d3722ed4ebf04092c30fd4b4dafff5b061a8384
Rismy Rally <rismyrally@gmail.com>
2020-07-01 17:49:56 +0530

    Dispatching Functions

--- src/index.js
+++ src/index.js
@@ -13,11 +13,19 @@ import { userAdded } from "./store/users";
 const store = configureStore();
 
 // const unsubscribe =
-store.subscribe(() => {
-  console.log("Store changed!", store.getState());
+// store.subscribe(() => {
+//   console.log("Store changed!", store.getState());
+// });
+
+store.dispatch((dispatch, getState) => {
+  // Call an API
+  // When the promise is resolved => dispatch()
+  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
+  console.log(getState());
+  // If the promise is rejected => dispatch()
 });
 
-store.dispatch(userAdded({ name: "User 1" }));
+// store.dispatch(userAdded({ name: "User 1" }));
 // store.dispatch(userAdded({ name: "User 2" }));
 // store.dispatch(projectAdded({ name: "Project 1" }));
 // store.dispatch(bugAdded({ description: "Bug 1" }));
@@ -29,8 +37,8 @@ store.dispatch(userAdded({ name: "User 1" }));
 
 // unsubscribe();
 
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-const bugs = getBugsByUser(1)(store.getState());
+// const unresolvedBugs = getUnresolvedBugs(store.getState());
+// const bugs = getBugsByUser(1)(store.getState());
 
-console.log(bugs);
+// console.log(bugs);
 // console.log(store.getState());
