75c9ac0f5bec3a35fff61a138377c5d11bf354a9
Rismy Rally <rismyrally@gmail.com>
2020-07-01 17:24:50 +0530

    Creating Middleware

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ store.subscribe(() => {
 });
 
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(bugRemoved({ id: 2 }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugRemoved({ id: 2 }));
 
 // unsubscribe();
 
